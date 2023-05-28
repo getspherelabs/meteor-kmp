44061bee15c6335ffceef90fa4d79b53888834ba
behzoddev <behzoddev@gmail.com>
2023-05-28 13:17:25 +0900

    build: Add coroutine and viewmodel dependencies

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -1,7 +1,13 @@
 object Version {
-
+    const val coroutine = "1.7.1"
+    const val viewModel = "2.2.0"
 }
 
 object Libs {
-
+    object Coroutine {
+        const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Version.coroutine}"
+    }
+    object Android {
+        const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Version.viewModel}"
+    }
 }
