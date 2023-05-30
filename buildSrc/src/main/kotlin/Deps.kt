a9980d084bcba5f9afc6cada1ea06658bc7d95c3
behzoddev <behzoddev@gmail.com>
2023-05-30 13:23:45 +0900

    build: Add the coroutine test dependency

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -2,12 +2,15 @@ object Version {
     const val coroutine = "1.7.1"
     const val viewModel = "2.2.0"
     const val ktlint = "10.3.0"
+    const val coroutineTest = "1.7.1"
 }
 
 object Libs {
     object Coroutine {
         const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Version.coroutine}"
+        const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:${Version.coroutineTest}"
     }
+
     object Android {
         const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Version.viewModel}"
     }
