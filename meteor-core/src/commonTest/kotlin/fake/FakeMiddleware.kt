70b50c2fa48495895a728d98595695650ac84047
behzoddev <behzoddev@gmail.com>
2023-05-30 13:25:08 +0900

    test: Add FakeMiddleware object

---
+++ meteor-core/src/commonTest/kotlin/fake/FakeMiddleware.kt
@@ -0,0 +1,25 @@
+package fake
+
+import io.spherelabs.meteor.Middleware
+import io.spherelabs.meteor.configs.To
+
+object FakeMiddleware : Middleware<FakeState, FakeWish, FakeEffect> {
+
+    override suspend fun process(
+        effect: To<FakeState, FakeEffect>,
+        wish: FakeWish,
+        next: suspend (FakeWish) -> Unit
+    ) {
+        when(wish)   {
+            FakeWish.Decrement -> {
+                effect.send(FakeEffect(message = "Decrement is not triggered"))
+                next(wish)
+            }
+            FakeWish.Increment -> {
+                next(wish)
+            }
+        }
+
+
+    }
+}
