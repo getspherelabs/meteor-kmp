1cb160d9738267029e1c72310c241898b8930807
behzoddev <behzoddev@gmail.com>
2023-05-30 13:26:01 +0900

    test: Add FakeMiddleware object, remove anonymous object

--- meteor-core/src/commonTest/kotlin/middleware/MiddlewareTest.kt
+++ meteor-core/src/commonTest/kotlin/middleware/MiddlewareTest.kt
@@ -1,38 +1,26 @@
 package middleware
 
-import io.spherelabs.meteor.Middleware
+import fake.FakeEffect
+import fake.FakeMiddleware
+import fake.FakeState
+import fake.FakeWish
 import io.spherelabs.meteor.configs.To
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class MiddlewareTest {
 
     @Test
-    fun `check the middleware works correctly`() = runBlocking {
-        val middleware = object: Middleware<Int, String, FakeMiddlewareEffect> {
-            override suspend fun process(
-                effect: To<Int, FakeMiddlewareEffect>,
-                wish: String,
-                next: suspend (String) -> Unit
-            ) {
-                val newWish = "Processed $wish"
+    fun `check the middleware works correctly`() = runTest {
+        var processedWish: FakeWish? = null
+        var processedEffect: FakeEffect? = null
 
-                next(newWish)
+        val nextFunction: suspend (FakeWish) -> Unit = { wish -> processedWish = wish }
+        val effectFunction: suspend (FakeEffect) -> Unit = { effect -> processedEffect = effect }
 
-                effect.send(FakeMiddlewareEffect("Effect is triggered"))
-            }
-        }
+        FakeMiddleware.process(To(effectFunction, FakeState()), FakeWish.Decrement, nextFunction)
 
-        var processedWish: String? = null
-        var processedEffect: FakeMiddlewareEffect? = null
-
-        val nextFunction: suspend (String) -> Unit = { wish -> processedWish = wish }
-        val effectFunction: suspend (FakeMiddlewareEffect) -> Unit = { effect -> processedEffect = effect }
-
-        middleware.process(To(effectFunction, 0), "Middleware", nextFunction)
-
-        assertEquals("Processed Middleware", processedWish)
-        assertEquals(FakeMiddlewareEffect("Effect is triggered"), processedEffect)
+        assertEquals(FakeEffect("Decrement is not triggered"), processedEffect)
     }
 }
