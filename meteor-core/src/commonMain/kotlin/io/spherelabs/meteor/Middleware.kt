bff20172a7690560bd7d1de43ddf778dff3d33a1
behzoddev <behzoddev@gmail.com>
2023-05-30 09:38:42 +0900

    feat: Add the middleware class

---
+++ meteor-core/src/commonMain/kotlin/io/spherelabs/meteor/Middleware.kt
@@ -0,0 +1,11 @@
+package io.spherelabs.meteor
+
+import io.spherelabs.meteor.configs.To
+
+/**
+ * [Middleware] sits between the source of wishes and the store,
+ * allowing for additional processing or side effects to occur before and after the state is updated.
+ */
+interface Middleware<State : Any, Wish : Any, Effect : Any> {
+    suspend fun process(effect: To<State, Effect>, wish: Wish, next: suspend (Wish) -> Unit)
+}
