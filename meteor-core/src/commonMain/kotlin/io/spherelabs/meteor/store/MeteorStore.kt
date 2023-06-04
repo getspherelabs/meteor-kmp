08264c6c964551903a3ff03de2333458d0a2a000
behzoddev <behzoddev@gmail.com>
2023-06-04 18:00:43 +0900

    refactor: Update meteor store

--- meteor-core/src/commonMain/kotlin/io/spherelabs/meteor/store/MeteorStore.kt
+++ meteor-core/src/commonMain/kotlin/io/spherelabs/meteor/store/MeteorStore.kt
@@ -1,22 +1,22 @@
 package io.spherelabs.meteor.store
 
-import io.spherelabs.meteor.Middleware
+import io.spherelabs.meteor.middleware.Middleware
 import io.spherelabs.meteor.configs.MeteorConfigs
 import io.spherelabs.meteor.configs.To
 import io.spherelabs.meteor.reducer.Reducer
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
-import kotlinx.coroutines.withContext
 
 class MeteorStore<State : Any, Wish : Any, Effect : Any>(
-    private val configs: MeteorConfigs<State>,
+    val configs: MeteorConfigs<State>,
     private val reducer: Reducer<State, Wish>,
     private val middleware: Middleware<State, Wish, Effect>,
     private val scope: CoroutineScope
@@ -27,28 +27,28 @@ class MeteorStore<State : Any, Wish : Any, Effect : Any>(
 
     override var currentState: State = configs.initialState
 
-    private val _effect: MutableSharedFlow<Effect> = MutableSharedFlow()
-    override val effect: Flow<Effect> = _effect.asSharedFlow()
+    private val _effect: Channel<Effect> = Channel()
+    override val effect: Flow<Effect> = _effect.receiveAsFlow()
 
     private val lock = Mutex()
 
     override suspend fun wish(wish: Wish) {
         lock.withLock {
-            withContext(configs.ioDispatcher) {
-                val oldState = _state.value
+            val oldState = _state.value
 
-                val newState = applyReducer(oldState, wish)
+            val newState = applyReducer(oldState, wish)
 
-                if (oldState != newState) {
-                    _state.value = newState
-                    currentState = newState
-                }
+            if (oldState != newState) {
+                _state.value = newState
+                currentState = newState
+            }
 
 
+            scope.launch {
                 middleware.process(
                     effect = To(
                         send = {
-                            _effect.tryEmit(it)
+                            _effect.send(it)
                         },
                         state = currentState
                     ), wish = wish,
