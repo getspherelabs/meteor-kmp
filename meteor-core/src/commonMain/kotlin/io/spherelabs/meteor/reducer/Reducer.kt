c4ea710c174cf0018d23e266bc1362ed5b8a6e61
behzoddev <behzoddev@gmail.com>
2023-06-06 13:45:34 +0900

    feat: Add expect and unexpected functions

--- meteor-core/src/commonMain/kotlin/io/spherelabs/meteor/reducer/Reducer.kt
+++ meteor-core/src/commonMain/kotlin/io/spherelabs/meteor/reducer/Reducer.kt
@@ -1,9 +1,21 @@
 package io.spherelabs.meteor.reducer
 
+import io.spherelabs.meteor.configs.To
+
 /**
  * A  [Reducer] are responsible for taking the current state and an wish,
  * and producing a new state based on the given inputs.
  */
-fun interface Reducer<State : Any, Wish : Any> {
-    fun reduce(state: State, wish: Wish): State
+fun interface Reducer<State : Any, Wish : Any, Effect : Any> {
+    fun reduce(state: State, wish: Wish): To<State, Effect>
+}
+
+public fun <State : Any, Wish : Any, Effect : Any> Reducer<State, Wish, Effect>.expect(
+    action: () -> State
+): To<State, Effect> {
+    return To(state = action())
+}
+
+public fun <State : Any, Wish : Any, Effect : Any> Reducer<State, Wish, Effect>.unexpected(): To<State, Effect> {
+    return To(null, null)
 }
