ed831b6165369abb9f0891acb515588d2c9f5278
Behzod Halil <behzoddev@gmail.com>
2023-06-08 13:45:07 +0900

    feat: Add meteor test extensions

---
+++ meteor-test/src/commonMain/kotlin/io/spherelabs/meteortest/store/MeteorTest.kt
@@ -0,0 +1,3 @@
+package io.spherelabs.meteortest.store
+
+
---
+++ meteor-test/src/commonMain/kotlin/io/spherelabs/meteortest/store/MeteorTestExtension.kt
@@ -0,0 +1,26 @@
+package io.spherelabs.meteortest.store
+
+import io.spherelabs.meteor.configs.MeteorConfigs
+import io.spherelabs.meteor.store.Store
+import io.spherelabs.meteor.store.createMeteor
+import kotlinx.coroutines.CoroutineScope
+
+fun <State : Any, Wish : Any, Effect : Any> createTestStore(
+    testConfigs: MeteorConfigs<State, Wish, Effect>,
+    testScope: CoroutineScope
+): Store<State, Wish, Effect> {
+    return createMeteor(
+        configs = testConfigs,
+        mainScope = testScope
+    )
+}
+
+fun <State : Any, Wish : Any, Effect : Any> CoroutineScope.createTestStore(
+    configs: MeteorConfigs<State, Wish, Effect>
+): Store<State, Wish, Effect> {
+    return createTestStore(
+        testConfigs = configs,
+        testScope = this
+    )
+}
+
