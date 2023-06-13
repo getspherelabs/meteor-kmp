e4f2cc9a9bc186f37aa4d390722544cd5eb1fda8
Behzod Halil <behzoddev@gmail.com>
2023-06-13 16:35:26 +0900

    feat: Add common flow

---
+++ meteor-viewmodel/src/commonMain/kotlin/io/spherelabs/meteorviewmodel/flow/CommonFlow.kt
@@ -0,0 +1,72 @@
+package io.spherelabs.meteorviewmodel.flow
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onEach
+
+public abstract class CommonFlow<out T>(private val flow: Flow<T>) : Flow<T> by flow {
+
+    public open fun watchFlow(
+        scope: CoroutineScope,
+        values: (T) -> Unit,
+        failure: (failure: Throwable) -> Unit,
+        completion: () -> Unit
+    ): Cancelable {
+        return flow.watch(scope, values, failure, completion)
+    }
+
+    public open fun watchFlow(
+        scope: CoroutineScope,
+        values: (T) -> Unit,
+    ): Cancelable {
+        return flow.watch(scope, values, null, null)
+    }
+
+    public open fun watchFlow(
+        scope: CoroutineScope,
+        values: (T) -> Unit,
+        failure: (failure: Throwable) -> Unit,
+    ): Cancelable {
+        return flow.watch(scope, values, failure, null)
+    }
+
+    public open fun watchFlow(
+        scope: CoroutineScope,
+        values: (T) -> Unit,
+        completion: () -> Unit
+    ): Cancelable {
+        return flow.watch(scope, values, null, completion)
+    }
+}
+
+
+internal fun <T> Flow<T>.watch(
+    scope: CoroutineScope,
+    values: (T) -> Unit,
+    failure: ((failure: Throwable) -> Unit)?,
+    completion: (() -> Unit)?
+): Cancelable {
+    val currentJob = this.onEach(values)
+        .run {
+            if (completion !== null) {
+                onCompletion { failure ->
+                    if (failure === null) completion() else throw failure
+                }
+            } else {
+                this
+            }
+        }
+        .run {
+            if (failure !== null) {
+                catch { message -> failure(message) }
+            } else {
+                this
+            }
+        }
+        .launchIn(scope)
+
+    return DefaultCancelable(currentJob)
+}
