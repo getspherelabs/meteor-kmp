ee61cf41e79be013e1320b01cdda79e9ac342cc7
Behzod Halil <behzoddev@gmail.com>
2023-06-13 16:36:01 +0900

    refactor: Fix not initialized exception bug

--- meteor-core/src/commonMain/kotlin/io/spherelabs/meteor/exception/NotInitializedException.kt
+++ meteor-core/src/commonMain/kotlin/io/spherelabs/meteor/exception/NotInitializedException.kt
@@ -1,3 +1,6 @@
 package io.spherelabs.meteor.exception
 
-class NotInitializedException(message: String = "State is not initialized.") : RuntimeException(message = message)
+class NotInitializedException : RuntimeException {
+    constructor()
+    constructor(message: String = "State is not initialized.")
+}
