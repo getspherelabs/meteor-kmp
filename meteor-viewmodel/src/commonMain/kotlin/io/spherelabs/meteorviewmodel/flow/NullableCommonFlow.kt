a8184861703e06bf399937c36c613097d193cb0e
Behzod Halil <behzoddev@gmail.com>
2023-06-13 16:35:46 +0900

    feat: Add nullable common flow

---
+++ meteor-viewmodel/src/commonMain/kotlin/io/spherelabs/meteorviewmodel/flow/NullableCommonFlow.kt
@@ -0,0 +1,14 @@
+package io.spherelabs.meteorviewmodel.flow
+
+import kotlinx.coroutines.flow.Flow
+import kotlin.experimental.ExperimentalObjCName
+import kotlin.native.ObjCName
+
+public open class NullableCommonFlow<out T : Any?> @OptIn(ExperimentalObjCName::class) constructor(
+    @ObjCName("_")
+    flow: Flow<T>
+) : CommonFlow<T>(flow)
+
+public fun <T> Flow<T>.wrap(): NullableCommonFlow<T> {
+    return this as? NullableCommonFlow<T> ?: NullableCommonFlow(this)
+}
