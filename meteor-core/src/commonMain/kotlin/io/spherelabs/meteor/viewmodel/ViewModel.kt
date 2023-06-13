a773ae628b8d29e9a0bdc93d8566eae0630186aa
Behzod Halil <behzoddev@gmail.com>
2023-06-13 17:37:47 +0900

    feat: Add viewModelScope to ViewModel

--- meteor-core/src/commonMain/kotlin/io/spherelabs/meteor/viewmodel/ViewModel.kt
+++
@@ -1,9 +0,0 @@
-package io.spherelabs.meteor.viewmodel
-
-import kotlinx.coroutines.CoroutineScope
-
-expect open class ViewModel() {
-    protected val viewModelScope: CoroutineScope
-
-    open fun onCleared()
-}
--- meteor-viewmodel/src/commonMain/kotlin/io/spherelabs/meteorviewmodel/core/ViewModel.kt
+++ meteor-viewmodel/src/commonMain/kotlin/io/spherelabs/meteorviewmodel/core/ViewModel.kt
@@ -1,10 +1,14 @@
 package io.spherelabs.meteorviewmodel.core
 
+import kotlinx.coroutines.CoroutineScope
+
 public expect abstract class ViewModel {
 
     public constructor()
 
     public constructor(vararg closeables: Closeable)
+
+    public val viewModelScope: CoroutineScope
 
     protected open fun onCleared()
 
