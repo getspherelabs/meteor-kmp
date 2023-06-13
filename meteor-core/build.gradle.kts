4e3234d76b7587557f04d31df3c766231c069eec
Behzod Halil <behzoddev@gmail.com>
2023-06-13 17:39:27 +0900

    feat: Add state and effect fields in CommonViewModel

--- meteor-core/build.gradle.kts
+++ meteor-core/build.gradle.kts
@@ -25,6 +25,7 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
+                implementation(project(":meteor-viewmodel"))
                 implementation(Libs.Coroutine.core)
             }
         }
--- meteor-core/src/commonMain/kotlin/io/spherelabs/meteor/viewmodel/CommonViewModel.kt
+++ meteor-core/src/commonMain/kotlin/io/spherelabs/meteor/viewmodel/CommonViewModel.kt
@@ -1,11 +1,23 @@
 package io.spherelabs.meteor.viewmodel
 
 import io.spherelabs.meteor.store.Store
+import io.spherelabs.meteorviewmodel.core.ViewModel
+import io.spherelabs.meteorviewmodel.flow.NonNullCommonFlow
+import io.spherelabs.meteorviewmodel.flow.NotNullCommonStateFlow
+import io.spherelabs.meteorviewmodel.flow.wrap
 import kotlinx.coroutines.launch
 
 abstract class CommonViewModel<State : Any, Wish : Any, Effect : Any> : ViewModel() {
 
     abstract val host: Store<State, Wish, Effect>
+
+    val effect: NonNullCommonFlow<Effect> by lazy {
+        host.effect.wrap()
+    }
+
+    val state: NotNullCommonStateFlow<State> by lazy {
+        host.state.wrap()
+    }
 
     fun wish(wish: Wish) {
         viewModelScope.launch {
