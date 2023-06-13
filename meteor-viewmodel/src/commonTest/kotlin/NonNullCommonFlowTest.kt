b588ba8364ed7802c8e963fe0cd320c5592d58b1
Behzod Halil <behzoddev@gmail.com>
2023-06-13 16:36:26 +0900

    test: Add non null common flow test

---
+++ meteor-viewmodel/src/commonTest/kotlin/NonNullCommonFlowTest.kt
@@ -0,0 +1,33 @@
+import io.spherelabs.meteorviewmodel.flow.NonNullCommonFlow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import kotlin.test.DefaultAsserter.assertNull
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
+
+class NonNullCommonFlowTest {
+
+    @Test
+    fun checkExecutesCommonFlowProperly() = runTest {
+        val numbers = mutableListOf<Int>()
+        var error = null as Throwable?
+        var completed = false
+
+        NonNullCommonFlow(flowOf(1, 2, 3, 4, 5))
+            .watchFlow(
+                scope = this,
+                values = {
+                    numbers.add(it)
+                },
+                failure = { error = it },
+                completion = { completed = true }
+            )
+            .join()
+
+        assertContentEquals(expected = listOf(1, 2, 3, 4, 5), actual = numbers)
+        assertTrue(completed)
+    }
+}
