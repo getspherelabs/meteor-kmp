98c4f91e0a28158c4a8ecf217152fbb0f236fdc6
Behzod Halil <behzoddev@gmail.com>
2023-06-13 16:34:36 +0900

    feat: Add viewmodel scope

---
+++ meteor-core/src/commonMain/kotlin/io/spherelabs/meteor/viewmodel/ViewModelScope.kt
@@ -0,0 +1,11 @@
+package io.spherelabs.meteor.viewmodel
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlin.native.concurrent.ThreadLocal
+
+@ThreadLocal
+var createViewModelScope: () -> CoroutineScope = {
+    CoroutineScope(Dispatchers.Main + SupervisorJob())
+}
