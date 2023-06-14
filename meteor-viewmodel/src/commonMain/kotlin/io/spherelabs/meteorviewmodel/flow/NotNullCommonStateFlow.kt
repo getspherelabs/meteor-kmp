276aa83f73f32042025eebdee929ac3edaf28a9b
Behzod Halil <behzoddev@gmail.com>
2023-06-14 17:51:13 +0900

    feat: Add NotNullCommonStateFlow

--- meteor-viewmodel/src/commonMain/kotlin/io/spherelabs/meteorviewmodel/flow/NotNullCommonStateFlow.kt
+++ meteor-viewmodel/src/commonMain/kotlin/io/spherelabs/meteorviewmodel/flow/NotNullCommonStateFlow.kt
@@ -2,13 +2,19 @@ package io.spherelabs.meteorviewmodel.flow
 
 import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.StateFlow
+import kotlin.experimental.ExperimentalObjCName
+import kotlin.native.ObjCName
 
-class NotNullCommonStateFlow<out T : Any> constructor(
+class NotNullCommonStateFlow<out T : Any> @OptIn(ExperimentalObjCName::class) constructor(
+    @ObjCName("_")
     private val flow: StateFlow<T>
 ) : NonNullCommonFlow<T>(flow), StateFlow<T> by flow {
-    override suspend fun collect(collector: FlowCollector<T>): Nothing = flow.collect(collector)
+    override suspend fun collect(collector: FlowCollector<T>): Nothing {
+        println("Collector $collector")
+        flow.collect(collector)
+    }
 }
 
-public fun <T : Any> StateFlow<T>.wrap(): NotNullCommonStateFlow<T> {
+public fun <T : Any> StateFlow<T>.asCommonFlow(): NotNullCommonStateFlow<T> {
     return this as? NotNullCommonStateFlow<T> ?: NotNullCommonStateFlow(this)
 }
