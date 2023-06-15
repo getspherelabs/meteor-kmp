d25eac1d138c6f755603421c46ba05b60231dafc
Behzod Halil <behzoddev@gmail.com>
2023-06-15 10:49:36 +0900

    chore: Delete unused files and move files #14

--- meteor-viewmodel/src/commonMain/kotlin/io/spherelabs/meteorviewmodel/Cancellable.kt
+++
@@ -1,5 +0,0 @@
-package io.spherelabs.meteorviewmodel
-
-interface Cancellable {
-    fun cancel()
-}
---
+++ meteor-viewmodel/src/iosMain/kotlin/io/spherelabs/meteorviewmodel/Closeable.kt
@@ -0,0 +1,5 @@
+package io.spherelabs.meteorviewmodel
+
+public actual fun interface Closeable {
+    public actual fun close()
+}
