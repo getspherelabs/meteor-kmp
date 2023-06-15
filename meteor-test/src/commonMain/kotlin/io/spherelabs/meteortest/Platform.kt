d7f8e3b84b9d8526720a209fee5f06ec631358d7
Behzod Halil <behzoddev@gmail.com>
2023-06-15 10:53:01 +0900

    chore: Delete unused files #14

--- meteor-test/src/commonMain/kotlin/io/spherelabs/meteortest/Platform.kt
+++
@@ -1,7 +0,0 @@
-package io.spherelabs.meteortest
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- meteor-viewmodel/src/iosMain/kotlin/io/spherelabs/meteorviewmodel/core/Closeable.ios.kt
+++
@@ -1,5 +0,0 @@
-package io.spherelabs.meteorviewmodel.core
-
-public actual fun interface Closeable {
-    public actual fun close()
-}
