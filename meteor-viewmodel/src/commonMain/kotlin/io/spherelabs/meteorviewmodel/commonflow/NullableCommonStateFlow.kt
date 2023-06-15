004cd3bc7f53bf3eb3d74d79e8edd4da2b437387
Behzod Halil <behzoddev@gmail.com>
2023-06-15 10:55:28 +0900

    chore: Add comments to nullable common state flow #14

---
+++ meteor-viewmodel/src/commonMain/kotlin/io/spherelabs/meteorviewmodel/commonflow/NullableCommonStateFlow.kt
@@ -0,0 +1,29 @@
+package io.spherelabs.meteorviewmodel.commonflow
+
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.StateFlow
+import kotlin.experimental.ExperimentalObjCName
+import kotlin.native.ObjCName
+
+/**
+ * [NullableCommonStateFlow] allows nullable values to be emitted by underlying [StateFlow].
+ *
+ * [ObjCName] instructs the Kotlin compiler to use a custom Objective-C and/or Swift name.
+ */
+
+@OptIn(ExperimentalObjCName::class)
+class NullableCommonStateFlow<T>  constructor(
+    @ObjCName("_")
+    private val flow: StateFlow<T>
+): NullableCommonFlow<T>(flow), StateFlow<T> by flow{
+
+    override suspend fun collect(collector: FlowCollector<T>): Nothing = flow.collect(collector)
+}
+
+/**
+ * Represents this a read-only state flow as nullable common state flow.
+ */
+
+public fun <T> StateFlow<T>.asCommonStateFlow(): NullableCommonStateFlow<T> {
+    return this as? NullableCommonStateFlow<T> ?: NullableCommonStateFlow(this)
+}
