4a4b64eb533c8152157e8a6cd12af8ef7bb0d695
Behzod Halil <behzoddev@gmail.com>
2023-06-15 10:54:43 +0900

    chore: Add comments to NonNullCommonFlow #14

---
+++ meteor-viewmodel/src/commonMain/kotlin/io/spherelabs/meteorviewmodel/commonflow/NonNullCommonFlow.kt
@@ -0,0 +1,38 @@
+package io.spherelabs.meteorviewmodel.commonflow
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlin.experimental.ExperimentalObjCName
+import kotlin.native.ObjCName
+
+/**
+ * A [NonNullCommonFlow] is specialized type of [CommonFlow] that guarantees non-null values emitted by the underlying flow.
+ * It extends the [CommonFlow] and provides additional safety by filtering out null values.
+ * [ObjCName] instructs the Kotlin compiler to use a custom Objective-C and/or Swift name.
+ */
+open class NonNullCommonFlow<out T : Any> @OptIn(ExperimentalObjCName::class) constructor(
+    @ObjCName("_")
+    flow: Flow<T>
+) : CommonFlow<T>(flow) {
+
+    /**
+     * Binds the flow to the given coroutine scope and
+     * invokes the provided callbacks for non-null values, failures, and completion.
+     * @return A CommonJob representing the binding of the flow to the given scope.
+     */
+    final override fun bind(
+        scope: CoroutineScope,
+        values: (T) -> Unit,
+        failure: ((failure: Throwable) -> Unit)?,
+        completion: (() -> Unit)?
+    ): CommonJob {
+        return super.bind(scope, values, failure, completion)
+    }
+
+}
+
+/**
+ * Represents this flow as a non-null common flow.
+ */
+public fun <T : Any> Flow<T>.asCommonFlow(): NonNullCommonFlow<T> =
+    this as? NonNullCommonFlow<T> ?: NonNullCommonFlow(this)
