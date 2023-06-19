177bb1ff4fe031fa4ed9be9e2034f0ba98768e19
Behzod Halil <behzoddev@gmail.com>
2023-06-19 17:47:25 +0900

    chore: Update gradle settings

--- meteor-core/build.gradle.kts
+++ meteor-core/build.gradle.kts
@@ -54,33 +54,24 @@ kotlin {
                 implementation(Libs.Coroutine.testJvm)
             }
         }
-        val iosX64Main by getting {
-            dependsOn(commonMain)
-        }
-        val iosArm64Main by getting {
-            dependsOn(commonMain)
-        }
-        val iosSimulatorArm64Main by getting {
-            dependsOn(commonMain)
-        }
+
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
         val iosMain by creating {
             dependsOn(commonMain)
-
-            dependencies {}
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
         }
-        val iosX64Test by getting {
-            dependsOn(commonTest)
-        }
-        val iosArm64Test by getting {
-            dependsOn(commonTest)
-        }
-        val iosSimulatorArm64Test by getting {
-            dependsOn(commonTest)
-        }
+        val iosX64Test by getting
+        val iosArm64Test by getting
+        val iosSimulatorArm64Test by getting
         val iosTest by creating {
             dependsOn(commonTest)
-
-            dependencies {}
+            iosX64Test.dependsOn(this)
+            iosArm64Test.dependsOn(this)
+            iosSimulatorArm64Test.dependsOn(this)
         }
     }
 }
