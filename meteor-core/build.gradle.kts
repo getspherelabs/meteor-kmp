3ffa97221270308724bf5f52b50bf2bbf8a7e98c
Behzod Halil <behzoddev@gmail.com>
2023-06-19 11:08:59 +0900

    build: Update gradle settings

--- meteor-core/build.gradle.kts
+++ meteor-core/build.gradle.kts
@@ -55,21 +55,11 @@ kotlin {
         val iosX64Main by getting
         val iosArm64Main by getting
         val iosSimulatorArm64Main by getting
-        val iosMain by creating {
-            dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
-        }
+        val iosMain by creating {}
         val iosX64Test by getting
         val iosArm64Test by getting
         val iosSimulatorArm64Test by getting
-        val iosTest by creating {
-            dependsOn(commonTest)
-            iosX64Test.dependsOn(this)
-            iosArm64Test.dependsOn(this)
-            iosSimulatorArm64Test.dependsOn(this)
-        }
+        val iosTest by creating {}
     }
 }
 
@@ -95,7 +85,9 @@ mavenPublishing {
 tasks.withType<org.jetbrains.dokka.gradle.DokkaTask>().configureEach {
     dokkaSourceSets {
         configureEach {
-            externalDocumentationLink("https://kotlinlang.org/api/kotlinx.coroutines/")
+            if (name.startsWith("ios")) {
+                displayName.set("ios")
+            }
 
             sourceLink {
                 localDirectory.set(projectDir.resolve("src"))
