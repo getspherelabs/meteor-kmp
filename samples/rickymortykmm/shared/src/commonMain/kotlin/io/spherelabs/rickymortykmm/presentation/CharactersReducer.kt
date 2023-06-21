adc940d7ebcea7d66ec017c8dda680a7c69716fb
Behzod Halil <behzoddev@gmail.com>
2023-06-21 15:40:22 +0900

    feat: Add character reducer

---
+++ samples/rickymortykmm/shared/src/commonMain/kotlin/io/spherelabs/rickymortykmm/presentation/CharactersReducer.kt
@@ -0,0 +1,30 @@
+package io.spherelabs.rickymortykmm.presentation
+
+import io.spherelabs.meteor.configs.Change
+import io.spherelabs.meteor.reducer.Reducer
+import io.spherelabs.meteor.reducer.effect
+import io.spherelabs.meteor.reducer.expect
+import io.spherelabs.meteor.reducer.unexpected
+
+object CharactersReducer : Reducer<CharactersState, CharactersWish, CharactersEffect> {
+
+    override fun reduce(state: CharactersState, wish: CharactersWish): Change<CharactersState, CharactersEffect> {
+        return when (wish) {
+            is CharactersWish.GetCharacters -> {
+                expect {
+                    state.copy(
+                        characters = wish.characters
+                    )
+                }
+            }
+            is CharactersWish.RequestFailed -> {
+                effect {
+                    CharactersEffect.Failure(wish.message)
+                }
+            }
+            else -> {
+                unexpected()
+            }
+        }
+    }
+}
