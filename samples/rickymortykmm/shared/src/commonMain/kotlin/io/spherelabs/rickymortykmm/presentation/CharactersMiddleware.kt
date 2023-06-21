37913562dc565539c4148f25156e23258ab4904c
Behzod Halil <behzoddev@gmail.com>
2023-06-21 15:40:38 +0900

    feat: Add character middleware

---
+++ samples/rickymortykmm/shared/src/commonMain/kotlin/io/spherelabs/rickymortykmm/presentation/CharactersMiddleware.kt
@@ -0,0 +1,27 @@
+package io.spherelabs.rickymortykmm.presentation
+
+import io.spherelabs.meteor.middleware.Middleware
+import io.spherelabs.rickymortykmm.domain.GetCharactersUseCase
+import kotlinx.coroutines.flow.collectLatest
+
+class CharactersMiddleware(
+    private val getCharactersUseCase: GetCharactersUseCase
+) : Middleware<CharactersWish> {
+
+    override suspend fun process(wish: CharactersWish, next: suspend (CharactersWish) -> Unit) {
+        when (wish) {
+            CharactersWish.CharacterStarted -> {
+                getCharactersUseCase.execute().collectLatest { result ->
+                    result.onSuccess {
+                        next.invoke(CharactersWish.GetCharacters(it.results))
+                    }.onFailure {
+                        val failureMsg = it.message ?: "Error is occurred."
+                        next.invoke(CharactersWish.RequestFailed(failureMsg))
+                    }
+                }
+            }
+            else -> {}
+        }
+
+    }
+}
