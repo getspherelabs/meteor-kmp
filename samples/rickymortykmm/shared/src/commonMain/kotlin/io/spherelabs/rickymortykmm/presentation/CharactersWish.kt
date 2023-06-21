fab99df7161b2485e90239230e5e83b1dd73d034
Behzod Halil <behzoddev@gmail.com>
2023-06-21 15:39:55 +0900

    feat: Add character wish

---
+++ samples/rickymortykmm/shared/src/commonMain/kotlin/io/spherelabs/rickymortykmm/presentation/CharactersWish.kt
@@ -0,0 +1,9 @@
+package io.spherelabs.rickymortykmm.presentation
+
+import io.spherelabs.rickymortykmm.remote.dto.CharacterDto
+
+sealed interface CharactersWish {
+    object CharacterStarted: CharactersWish
+    data class GetCharacters(val characters: List<CharacterDto>) : CharactersWish
+    data class RequestFailed(val message: String): CharactersWish
+}
