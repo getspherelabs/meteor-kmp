d024557945f76cd3ec89e768b8d88dd5d5d52326
behzoddev <behzoddev@gmail.com>
2023-06-21 23:53:23 +0900

    feat: Add  character screen

---
+++ samples/rickymortykmm/androidApp/src/main/java/io/spherelabs/rickymortykmm/android/ui/CharacterScreen.kt
@@ -0,0 +1,50 @@
+package io.spherelabs.rickymortykmm.android.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import io.spherelabs.rickymortykmm.presentation.CharactersEffect
+import io.spherelabs.rickymortykmm.presentation.CharactersState
+import io.spherelabs.rickymortykmm.presentation.CharactersWish
+import io.spherelabs.rickymortykmm.remote.dto.CharacterDto
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+
+@Composable
+fun CharacterScreen(
+    modifier: Modifier = Modifier,
+    wish: (CharactersWish) -> Unit,
+    state: StateFlow<CharactersState>,
+    effect: Flow<CharactersEffect>
+) {
+    val uiState = state.collectAsState()
+
+    LaunchedEffect(key1 = true) {
+        effect.collectLatest {
+
+        }
+    }
+
+    Column(modifier = modifier.fillMaxSize()) {
+        Characters(characters = uiState.value.characters)
+    }
+
+}
+
+@Composable
+fun Characters(
+    modifier: Modifier = Modifier,
+    characters: List<CharacterDto>
+) {
+    LazyColumn(modifier = modifier.fillMaxSize()) {
+        items(characters) { character ->
+            CharacterItem(character = character)
+        }
+    }
+}
