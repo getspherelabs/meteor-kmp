cded543f9147a63d996187c4d0b999206f4580b5
behzoddev <behzoddev@gmail.com>
2023-06-25 23:32:28 +0900

    refactor: Add effect for getting detail

--- samples/rickymortykmm/shared/src/commonMain/kotlin/io/spherelabs/rickymortykmm/presentation/CharactersEffect.kt
+++ samples/rickymortykmm/shared/src/commonMain/kotlin/io/spherelabs/rickymortykmm/presentation/CharactersEffect.kt
@@ -2,4 +2,5 @@ package io.spherelabs.rickymortykmm.presentation
 
 sealed interface CharactersEffect {
     data class Failure(val message: String) : CharactersEffect
+    data class GetDetail(val id: Int): CharactersEffect
 }
