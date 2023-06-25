b8a446d172ab5e0c77f4d11927abf3f32bbe39d4
behzoddev <behzoddev@gmail.com>
2023-06-25 23:30:37 +0900

    feat: Add reducer for character detail

---
+++ samples/rickymortykmm/shared/src/commonMain/kotlin/io/spherelabs/rickymortykmm/presentation/detail/CharacterDetailReducer.kt
@@ -0,0 +1,38 @@
+package io.spherelabs.rickymortykmm.presentation.detail
+
+import io.spherelabs.meteor.configs.Change
+import io.spherelabs.meteor.reducer.Reducer
+import io.spherelabs.meteor.reducer.expect
+import io.spherelabs.meteor.reducer.unexpected
+
+object CharacterDetailReducer : Reducer<CharacterDetailState, CharacterDetailWish, CharacterDetailEffect> {
+
+    override fun reduce(state: CharacterDetailState, wish: CharacterDetailWish): Change<CharacterDetailState, CharacterDetailEffect> {
+        return when (wish) {
+            is CharacterDetailWish.GettingCompleted -> {
+                expect {
+                    state.copy(
+                        isLoading = false,
+                        character = wish.character
+                    )
+                }
+            }
+
+            is CharacterDetailWish.Toast -> {
+                val newState = state.copy(isLoading = false)
+                val newEffect = CharacterDetailEffect.Failure(wish.msg)
+
+                Change(effect = newEffect, state = newState)
+            }
+
+            is  CharacterDetailWish.GettingLoading -> {
+                expect {
+                    state.copy(isLoading = true)
+                }
+            }
+            else -> {
+                unexpected { state }
+            }
+        }
+    }
+}
