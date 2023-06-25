fce2cb92bf2ff8fcdb36d617c4e21f16047044af
behzoddev <behzoddev@gmail.com>
2023-06-25 23:31:09 +0900

    feat: Add viewmodel for character detail

---
+++ samples/rickymortykmm/shared/src/commonMain/kotlin/io/spherelabs/rickymortykmm/presentation/detail/CharacterDetailViewModel.kt
@@ -0,0 +1,25 @@
+package io.spherelabs.rickymortykmm.presentation.detail
+
+import io.spherelabs.meteor.configs.MeteorConfigs
+import io.spherelabs.meteor.store.Store
+import io.spherelabs.meteor.viewmodel.CommonViewModel
+import io.spherelabs.meteor.viewmodel.createMeteor
+import io.spherelabs.meteorviewmodel.commonflow.NonNullCommonFlow
+import io.spherelabs.meteorviewmodel.commonflow.NonNullCommonStateFlow
+import io.spherelabs.meteorviewmodel.commonflow.asCommonFlow
+import io.spherelabs.meteorviewmodel.commonflow.asCommonStateFlow
+
+class CharacterDetailViewModel: CommonViewModel<CharacterDetailState, CharacterDetailWish, CharacterDetailEffect>() {
+
+    override val store: Store<CharacterDetailState, CharacterDetailWish, CharacterDetailEffect> = createMeteor(
+        configs = MeteorConfigs.build {
+            initialState = CharacterDetailState.Empty
+            storeName = "character_detail_store"
+            reducer = CharacterDetailReducer
+            middleware = CharacterDetailMiddleware
+        }
+    )
+
+    val state: NonNullCommonStateFlow<CharacterDetailState> = store.state.asCommonStateFlow()
+    val effect: NonNullCommonFlow<CharacterDetailEffect> = store.effect.asCommonFlow()
+}
