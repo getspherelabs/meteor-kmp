7a66a4fabef1e54ce8b512370d136e7cb319ef2f
behzoddev <behzoddev@gmail.com>
2023-06-25 23:32:03 +0900

    feat: Add character route to handle easily

--- samples/rickymortykmm/androidApp/src/main/java/io/spherelabs/rickymortykmm/android/ui/CharacterScreen.kt
+++ samples/rickymortykmm/androidApp/src/main/java/io/spherelabs/rickymortykmm/android/ui/CharacterScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import io.spherelabs.rickymortykmm.presentation.CharactersEffect
 import io.spherelabs.rickymortykmm.presentation.CharactersState
+import io.spherelabs.rickymortykmm.presentation.CharactersViewModel
 import io.spherelabs.rickymortykmm.presentation.CharactersWish
 import io.spherelabs.rickymortykmm.remote.dto.CharacterDto
 import kotlinx.coroutines.flow.Flow
@@ -17,33 +18,63 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collectLatest
 
 @Composable
+fun CharactersRoute(
+    viewModel: CharactersViewModel,
+    navigateToDetail: (Int) -> Unit
+) {
+    CharacterScreen(
+        wish = {
+            viewModel.wish(it)
+        },
+        state = viewModel.state,
+        effect = viewModel.effect,
+        routeToDetail = {
+            navigateToDetail.invoke(it)
+        }
+    )
+}
+
+@Composable
 fun CharacterScreen(
     modifier: Modifier = Modifier,
     wish: (CharactersWish) -> Unit,
     state: StateFlow<CharactersState>,
-    effect: Flow<CharactersEffect>
+    effect: Flow<CharactersEffect>,
+    routeToDetail: (Int) -> Unit
 ) {
     val uiState = state.collectAsState()
 
     LaunchedEffect(key1 = true) {
         wish(CharactersWish.CharacterStarted)
-        effect.collectLatest {
+        effect.collectLatest { effectResult ->
+            when (effectResult) {
+                is CharactersEffect.Failure -> {}
+                is CharactersEffect.GetDetail -> {
+                    routeToDetail(effectResult.id)
+                }
+            }
         }
     }
 
     Column(modifier = modifier.fillMaxSize()) {
-        Characters(characters = uiState.value.characters)
+        Characters(
+            characters = uiState.value.characters,
+            onDetailClick = { newId ->
+                wish.invoke(CharactersWish.GetDetail(newId))
+            }
+        )
     }
 }
 
 @Composable
 fun Characters(
     modifier: Modifier = Modifier,
-    characters: List<CharacterDto>
+    characters: List<CharacterDto>,
+    onDetailClick: (Int) -> Unit
 ) {
     LazyColumn(modifier = modifier.fillMaxSize()) {
         items(characters) { character ->
-            CharacterItem(character = character)
+            CharacterItem(character = character, onClick = { onDetailClick(character.id) })
         }
     }
 }
