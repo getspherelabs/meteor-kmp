137d05b7b6f2e3c8bf1e30858ae40d9cd3db69d0
behzoddev <behzoddev@gmail.com>
2023-07-31 21:42:41 +0900

    feat: Add coroutine test middleware

--- meteor-core/src/commonTest/kotlin/fake/FakeCountMiddleware.kt
+++ meteor-core/src/commonTest/kotlin/fake/FakeCountMiddleware.kt
@@ -1,14 +1,19 @@
 package fake
 
+import io.spherelabs.meteor.middleware.CoroutineMiddleware
 import io.spherelabs.meteor.middleware.Middleware
 import io.spherelabs.meteorlogger.log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.supervisorScope
 
-fun countMiddleware() = object : Middleware<FakeCountState, FakeCountWish> {
-    override suspend fun process(state: FakeCountState, wish: FakeCountWish, next: suspend (FakeCountWish) -> Unit) {
+fun countMiddleware() = object : CoroutineMiddleware<FakeCountState, FakeCountWish> {
+    override suspend fun CoroutineScope.handle(state: FakeCountState, wish: FakeCountWish, next: suspend (FakeCountWish) -> Unit) {
         when (wish) {
             FakeCountWish.Decrease -> {
-                if (state.count == 0) {
-                    next(FakeCountWish.ZeroValue)
+                supervisorScope {
+                    if (state.count == 0) {
+                        next(FakeCountWish.ZeroValue)
+                    }
                 }
             }
 
